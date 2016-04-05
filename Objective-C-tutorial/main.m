46b203fefb1b595489cbc3a0a3e88127893ff400
Reinis Riekstins <rr14022@lu.lv>
2016-04-05 20:17:13 +0300

    Initial Commit

---
+++ Objective-C-tutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-tutorial
+//
+//  Created by Reinis Riekstins on 05/04/16.
+//  Copyright © 2016 ReinisRiekstins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
